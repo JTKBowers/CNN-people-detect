129027ee6f72dba0c9fca22bf5aa8a5fa16a8b26
Jamie <jtb56@uclive.ac.nz>
2016-04-23 19:32:55 +1200

    Further parameterised network output size, increased input resolution and decreased batch size to avoid OOM errors.

--- train_model.py
+++ train_model.py
@@ -67,9 +67,9 @@ def dropout(h_fc1):
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
     return keep_prob, h_fc1_drop
 
-def build_readout_layer(h_fc1_drop):
-    W_fc2 = weight_variable([1024, 100])
-    b_fc2 = bias_variable([100])
+def build_readout_layer(h_fc1_drop, output_size):
+    W_fc2 = weight_variable([1024, output_size])
+    b_fc2 = bias_variable([output_size])
 
     y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
     return y_conv
@@ -83,10 +83,10 @@ if __name__ == '__main__':
     print(len(combined_dataset.train), 'training examples.')
     print(len(combined_dataset.test), 'testing examples.')
 
-    nn_im_w = 100
-    nn_im_h = 100
-    nn_out_w = 10
-    nn_out_h = 10
+    nn_im_w = 320
+    nn_im_h = 240
+    nn_out_w = 32
+    nn_out_h = 32
     with tf.Session() as sess:
         with tf.device('/cpu:0'):
             x = tf.placeholder(tf.float32, shape=[None, nn_im_w*nn_im_h*3])
@@ -96,7 +96,7 @@ if __name__ == '__main__':
             h_pool2 = build_layer_2(h_pool1)
             h_fc1 = fully_connected_layer(h_pool2, nn_im_w//4, nn_im_h//4) #/4 because of two 2x2 pooling layers
             keep_prob, h_fc1_drop = dropout(h_fc1)
-            y_conv = build_readout_layer(h_fc1_drop) #vector of classes - length 10
+            y_conv = build_readout_layer(h_fc1_drop, nn_out_w*nn_out_h)
 
             cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
             train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
@@ -105,16 +105,16 @@ if __name__ == '__main__':
         sess.run(tf.initialize_all_variables())
 
 
-        for batch_no, batch in enumerate(combined_dataset.train.iter_batches(nn_im_w, nn_im_h, nn_out_w,nn_out_h)):
+        for batch_no, batch in enumerate(combined_dataset.train.iter_batches(nn_im_w, nn_im_h, nn_out_w,nn_out_h, batch_size=10)):
             train_accuracy = accuracy.eval(feed_dict={
                 x:batch[0], y_: batch[1], keep_prob: 1.0})
-            print(train_accuracy)
-            print("step %d, training accuracy %g"%(batch_no, train_accuracy))
+            if batch_no % 5 == 0:
+                print("step %d, training accuracy %g"%(batch_no, train_accuracy))
             train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
         cum_accuracy = 0
         num_batches = 0
-        for batch in combined_dataset.test.iter_batches(nn_im_w, nn_im_h,nn_out_w,nn_out_h):
+        for batch in combined_dataset.test.iter_batches(nn_im_w, nn_im_h,nn_out_w,nn_out_h, batch_size=10):
             cum_accuracy += accuracy.eval(feed_dict={
                 x: batch[0], y_: batch[1], keep_prob: 1.0})
             num_batches += 1
