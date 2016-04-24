097fb8156b0db5260350b81dd54d3631f2c0c9ee
Jamie <jtb56@uclive.ac.nz>
2016-04-24 18:11:50 +1200

    Changed network error and normalisation functions, saved the weights of the first layer, added code to display a test image and its result, and code to illustrate the distribution of the output.

--- train_model.py
+++ train_model.py
@@ -8,6 +8,7 @@ Trains a convolutional neural network to detect people. The training set used is
 # and tensorflow loads an incompatible internal version of libpng
 # https://github.com/tensorflow/tensorflow/issues/1924
 import cv2
+import numpy as np
 import tensorflow as tf
 
 from Datasets.tud import loadTUD
@@ -39,7 +40,7 @@ def build_layer_1(x, im_w, im_h):
     x_image = tf.reshape(x, [-1,im_w,im_h,num_colour_channels])
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
-    return h_pool1
+    return h_pool1, W_conv1, b_conv1
 
 def build_layer_2(h_pool1):
     patch_size = 5
@@ -53,7 +54,7 @@ def build_layer_2(h_pool1):
     return h_pool2
 
 def fully_connected_layer(h_pool2, im_w, im_h):
-    num_neurons = 1024
+    num_neurons = 512
     input_channels = 64
     W_fc1 = weight_variable([im_w * im_h * input_channels, num_neurons])
     b_fc1 = bias_variable([num_neurons])
@@ -68,16 +69,22 @@ def dropout(h_fc1):
     return keep_prob, h_fc1_drop
 
 def build_readout_layer(h_fc1_drop, output_size):
-    W_fc2 = weight_variable([1024, output_size])
+    W_fc2 = weight_variable([512, output_size])
     b_fc2 = bias_variable([output_size])
 
-    y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+    #y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+    #y_conv=tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+    #y_conv=tf.nn.l2_normalize(tf.matmul(h_fc1_drop, W_fc2) + b_fc2, dim=1)
+    y_raw=tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+    y_conv = (y_raw + 1)/2
     return y_conv
 
 if __name__ == '__main__':
     combined_dataset = loadTUD('/mnt/pedestrians/tud/tud-pedestrians') + \
           loadTUD('/mnt/pedestrians/tud/tud-campus-sequence') + \
           loadTUD('/mnt/pedestrians/tud/TUD-Brussels') + \
+          loadTUD('/mnt/pedestrians/tud/train-210') + \
+          loadTUD('/mnt/pedestrians/tud/train-400') + \
           loadINRIA('/mnt/pedestrians/INRIA/INRIAPerson')
     print(len(combined_dataset.train), 'training examples.')
     print(len(combined_dataset.test), 'testing examples.')
@@ -91,24 +98,34 @@ if __name__ == '__main__':
             x = tf.placeholder(tf.float32, shape=[None, nn_im_w*nn_im_h*3])
             y_ = tf.placeholder(tf.float32, shape=[None, nn_out_w*nn_out_h])
 
-            h_pool1 = build_layer_1(x, nn_im_w, nn_im_h)
+            h_pool1, W_conv1, b_conv1 = build_layer_1(x, nn_im_w, nn_im_h)
             h_pool2 = build_layer_2(h_pool1)
             h_fc1 = fully_connected_layer(h_pool2, nn_im_w//4, nn_im_h//4) #/4 because of two 2x2 pooling layers
             keep_prob, h_fc1_drop = dropout(h_fc1)
             y_conv = build_readout_layer(h_fc1_drop, nn_out_w*nn_out_h)
 
-            cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
-            train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+            # See http://quabr.com/33712178/tensorflow-nan-bug for reasons for clipping.
+            ##cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(tf.clip_by_value(y_conv,1e-10,1.0)), reduction_indices=[1]))
+            #cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+
+            # Sum squared differences between the example and calculated images (but do not sum over the batch dimension).
+            ssd_images = tf.reduce_sum(tf.square(y_ - y_conv), [1])
+            mean_error = tf.reduce_mean(ssd_images) # average image sum of squares across all batches.
+            train_step = tf.train.AdamOptimizer(1e-4).minimize(mean_error)
             correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
             accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
         sess.run(tf.initialize_all_variables())
-
 
         for batch_no, batch in enumerate(combined_dataset.train.iter_batches(nn_im_w, nn_im_h, nn_out_w,nn_out_h, batch_size=10)):
             train_accuracy = accuracy.eval(feed_dict={
                 x:batch[0], y_: batch[1], keep_prob: 1.0})
             if batch_no % 5 == 0:
                 print("step %d, training accuracy %g"%(batch_no, train_accuracy))
+                r = y_conv.eval(feed_dict={x: batch[0], keep_prob: 1.0})
+                print('Input Min:',np.min(batch[0]), 'Avg:', np.mean(batch[0]), 'Max:', np.max(batch[0]))
+                print('Y Min:',np.min(batch[1]), 'Avg:', np.mean(batch[1]), 'Max:', np.max(batch[1]))
+                print('Y_Conv Min:',np.min(r), 'Avg:', np.mean(r), 'Max:', np.max(r))
+
             train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
         cum_accuracy = 0
@@ -119,3 +136,23 @@ if __name__ == '__main__':
             num_batches += 1
         mean_accuracy = cum_accuracy/num_batches
         print("test accuracy %g"%mean_accuracy)
+
+        # display an image
+        cv2.namedWindow('Input')
+        cv2.namedWindow('Output')
+        im, y = next(combined_dataset.test.iter(nn_im_w,nn_im_h, nn_out_w, nn_out_h, normalize=False))
+        y = y_conv.eval(feed_dict={x: im, keep_prob: 1.0})
+        np.save('y.npy', y)
+
+        im = im.reshape((nn_im_h,nn_im_w, 3)).astype(np.uint8)
+        y = (255*y).reshape((nn_out_h,nn_out_w)).astype(np.uint8)
+        cv2.imshow('Input',im)
+        cv2.imshow('Output',y)
+        cv2.imwrite('out.png', y)
+        print(y)
+        cv2.waitKey()
+
+        # save model:
+        W1_val = sess.run(W_conv1)
+        np.save('W1.npy', W1_val)
+        np.save('y.npy', y)
