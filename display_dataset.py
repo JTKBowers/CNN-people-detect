a89ba0c31c5ab7cb7f7cdd9aeef9dc646fb5f3a4
Jamie <jtb56@uclive.ac.nz>
2016-05-15 02:46:13 +1200

    Added TUD-crossing and TUD-MotionPairs datasets.

--- display_dataset.py
+++ display_dataset.py
@@ -8,9 +8,12 @@ from Datasets.Zurich import loadZurich
 if __name__ == '__main__':
     combined_dataset = loadTUD('/mnt/data/Datasets/pedestrians/tud/tud-pedestrians') + \
           loadTUD('/mnt/data/Datasets/pedestrians/tud/tud-campus-sequence') + \
+          loadTUD('/mnt/data/Datasets/pedestrians/tud/tud-crossing-sequence') + \
           loadTUD('/mnt/data/Datasets/pedestrians/tud/TUD-Brussels') + \
           loadTUD('/mnt/data/Datasets/pedestrians/tud/train-210') + \
           loadTUD('/mnt/data/Datasets/pedestrians/tud/train-400') + \
+          loadTUD('/mnt/data/Datasets/pedestrians/tud/TUD-MotionPairs/positive') + \
+          loadTUD('/mnt/data/Datasets/pedestrians/tud/TUD-MotionPairs/negative') + \
           loadINRIA('/mnt/data/Datasets/pedestrians/INRIA/INRIAPerson') + \
           loadZurich('/mnt/data/Datasets/pedestrians/zurich')
 
